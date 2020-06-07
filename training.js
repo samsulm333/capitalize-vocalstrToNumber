bcf24a363b1db3bb575fccc0bdcc4c855283f6da
samsul <samsul.m333@gmail.com>
2020-06-07 23:46:29 +0700

    update soal 4

--- training.js
+++ training.js
@@ -64,3 +64,59 @@ breakRecords([50, 30, 65, 20, 10, 50, 60]);
 
 
 
+//soal 4
+let karyawanList = [];
+
+function Karyawan(id, nama, usia, divisi){
+    this.nomorId = id;
+    this.nama = nama;
+    this.usia = usia;
+    this.divisi = divisi;
+}
+
+function tambahDataKaryawan(nama, usia, divisi){
+    karyawanList.push(new Karyawan(karyawanList.length+1, nama, usia, divisi));
+} 
+
+function findKaryawan(id){
+     for (i= 0; i<karyawanList.length; i++){
+         if (karyawanList[i].nomorId == id){
+             return karyawanList[i];
+        }            
+    }
+}
+
+function hapusDataKaryawan(id){
+    for (var i = 0; i < karyawanList.length; i++){
+        if (karyawanList[i].nomorId == id){
+            delete karyawanList[i];
+        }
+    }
+}
+
+function updateDataKaryawan(id, pindahDivisi){
+    for (var i = 0; i< karyawanList.length; i++){
+        if (karyawanList[i].nomorId == id){
+            return karyawanList[i].divisi = pindahDivisi;
+        }
+    }
+}
+
+tambahDataKaryawan('Dora', 25, 'Marketing');
+tambahDataKaryawan('Emon', 27, 'Finance');
+tambahDataKaryawan('joni', 24, 'Logistic');
+console.log(karyawanList);
+
+console.log(findKaryawan(1));
+
+// console.log(hapusDataKaryawan(1));
+
+// console.log(karyawanList);
+
+updateDataKaryawan(1,'Acounting'); //sorry kak ini saya lupa untuk yang di update apanya jadi saya improove aja,
+console.log(karyawanList);
+
+
+
+
+
