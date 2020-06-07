3744a7d9775be538dd6e910b27e48ff4433970dd
samsul <samsul.m333@gmail.com>
2020-06-07 11:35:37 +0700

    update soal2

--- training.js
+++ training.js
@@ -8,11 +8,27 @@ function tarifP(masuk , keluar){
     var durasi = (parseInt(keluar[0]) - parseInt(masuk[0])) + (Math.ceil((parseInt(keluar[1]) - parseInt(masuk[1]))/60));
     
     var bayar = durasi * perJam + jamPertama;
-    
         
-    
     return bayar;
 }
 
-
 console.log(tarifP('11:30', '16:00'));
+
+
+///Soal 2
+
+function setAlarm(set){
+    var now = new Date().getHours() +':'+ new Date().getMinutes();
+    var arrNow = now.split(':');
+    var set = set.split(':');
+
+    durasi = parseInt((set[0] - arrNow[0])*60 + (set[1] - arrNow[1])); 
+
+    jam  = Math.floor(durasi/60);
+    menit = durasi%60;
+    
+    console.log(`alarm di set dalam ${jam} jam dan ${menit} menit. `)
+    
+}
+
+setAlarm('13:22');
