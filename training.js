042260d213ea1e396734b8ec9a48cce37c4a44d7
samsul <samsul.m333@gmail.com>
2020-06-07 14:51:40 +0700

    update

--- training.js
+++ training.js
@@ -1,18 +1,18 @@
 ///soal 1
 
-function tarifP(masuk , keluar){
-    var jamPertama = 5000;
-    var perJam = 4000;
-    var masuk = masuk.split(':');
-    var keluar = keluar.split(':');
-    var durasi = (parseInt(keluar[0]) - parseInt(masuk[0])) + (Math.ceil((parseInt(keluar[1]) - parseInt(masuk[1]))/60));
+// function tarifP(masuk , keluar){
+//     var jamPertama = 5000;
+//     var perJam = 4000;
+//     var masuk = masuk.split(':');
+//     var keluar = keluar.split(':');
+//     var durasi = (parseInt(keluar[0]) - parseInt(masuk[0])) + (Math.ceil((parseInt(keluar[1]) - parseInt(masuk[1]))/60));
     
-    var bayar = durasi * perJam + jamPertama;
+//     var bayar = durasi * perJam + jamPertama;
         
-    return bayar;
-}
+//     return bayar;
+// }
 
-console.log(tarifP('11:30', '16:00'));
+// console.log(tarifP('11:30', '16:00'));
 
 
 ///Soal 2
@@ -24,6 +24,7 @@ function setAlarm(set){
 
     durasi = parseInt((set[0] - arrNow[0])*60 + (set[1] - arrNow[1])); 
 
+    // jam  = durasi/60;
     jam  = Math.floor(durasi/60);
     menit = durasi%60;
     
@@ -31,4 +32,35 @@ function setAlarm(set){
     
 }
 
-setAlarm('13:22');
+setAlarm('17:10');
+
+
+
+
+///soal 3
+
+function breakRecords(arr){
+    let max = arr[0];
+    let min = arr[0];
+    
+    var countMax = 0;
+    var countMin = 0;
+
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i]>max) {
+            max = arr[i];
+             countMax++;
+        }
+        else if(arr[i]< min){
+            min = arr[i];
+             countMin++;
+        }
+    }
+    
+    console.log(`Record Max: ${max} ${countMax} times, Record Min : ${min} ${countMin} times.`);
+}
+breakRecords([50, 30, 65, 20, 10, 50, 60]);
+
+
+
+
